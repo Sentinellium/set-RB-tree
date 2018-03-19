93335985aad33eee3460de8c0e88a49cc5e51933
Sentinellium <34355821+Sentinellium@users.noreply.github.com>
2018-03-19 07:04:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -439,7 +439,7 @@ void RBtree::display(node *p)
      cout<<"\nNo Right Child.\n"*/
      }
 }
-/*
+
 void RBtree::Union(node *p){
     int *m = new int;
     int *k = new int;
@@ -470,7 +470,7 @@ void RBtree::Union(node *p){
     delete []first;
     delete []second;
 }
-*/
+
 
 void RBtree::Intersection(node *p){
     int *m=new int;
@@ -567,42 +567,5 @@ int main()
         obj.insert(rand()%10);
         obk.insert(rand()%10);
     }
-    //obj.disp();
-    cout <<endl;
-        cout <<endl;
-            cout <<endl;
-    //obk.disp();
-    cout <<endl;
-        cout <<endl;
-            cout <<endl;
-    obj.Intersection(obk.root);
-    do
-    {
-                cout<<"\n\t RED BLACK TREE " ;
-                cout<<"\n 1. Insert in the tree ";
-                cout<<"\n 2. Delete a node from the tree";
-                cout<<"\n 3. Search for an element in the tree";
-                cout<<"\n 4. Display the tree ";
-                cout<<"\n 5. Exit " ;
-                cout<<"\nEnter Your Choice: ";
-                cin>>ch;
-                switch(ch)
-                {
-                          //case 1 : obj.insert();
-                                   //cout<<"\nNode Inserted.\n";
-                                   //break;
-                          case 2 : obj.del();
-                                   break;
-                          case 3 : obj.search();
-                                   break;
-                          case 4 : obj.disp();
-                                   break;
-                          case 5 : y=1;
-                                   break;
-                          default : cout<<"\nEnter a Valid Choice.";
-                }
-                cout<<endl;
-
-    }while(y!=1);
-    return 1;
+    
 }
